921ccc731440fd07bb40e8418b516cef28807ce8
Beking0912 <sbqoo@qq.com>
2023-06-15 16:21:52 -0400

    add meta info

--- nuxt3project/nuxt.config.ts
+++ nuxt3project/nuxt.config.ts
@@ -1,4 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const isDev = process.env.NODE_ENV === 'development'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: isDev },
+
+  app: {
+    head: {
+      titleTemplate: "%s - Bullwhip",
+      title: "Bullwhip",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui",
+        },
+        {
+          name: "description",
+          content: process.env.npm_package_description || "",
+        },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+});
