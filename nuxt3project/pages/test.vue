b8358360e96041f88992a0d32186b0229070b5e3
Beking0912 <sbqoo@qq.com>
2023-07-14 12:54:35 -0400

    add fetch data

--- nuxt3project/pages/test.vue
+++ nuxt3project/pages/test.vue
@@ -3,9 +3,31 @@
     <NuxtLink to="/">
       index page
     </NuxtLink>
+
+    <img v-if="pending" :src="'assets/images/open_peeps.png'">
+    <div v-else>{{ data.length }}</div>
+
+    <p>
+      {{ name }} {{ doubleCount }}
+    </p>
+    <v-btn @click="increment">
+      count++
+    </v-btn>
   </div>
 </template>
 <script setup>
+const store = useCounterStore()
+const { name, doubleCount } = storeToRefs(store)
+const { increment } = store
+
+const { pending, data } = await useLazyAsyncData(
+  'mountains',
+  () => $fetch('https://api.nuxtjs.dev/mountains'),
+  {
+    watch: [doubleCount]
+  }
+)
+
 definePageMeta({
   layout: 'blank'
 })
